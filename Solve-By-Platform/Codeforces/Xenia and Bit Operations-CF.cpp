4c671bbb8eafbb6d5ecc0defee760be75e338ad1
elvengard <rifat.cse4.bu@gmail.com>
2020-02-07 12:35:09 +0600

    Segment Tree

---
+++ Solve-By-Platform/Codeforces/Xenia and Bit Operations-CF.cpp
@@ -0,0 +1,105 @@
+/*******Bismillahir-Rahmanir-Rahim**********/
+/* 
+Name         : Abu Rifat Muhammed Al Hasib 
+Institution  : University of Barisal, Bangladesh
+Github       : abu-rifat
+Codeforces   : AbuRifatMuhammed
+AtCoder      : aburifat
+codechef     : aburifat
+SPOJ         : abu_rifat
+UvaOJ        : aburifat
+DevSkill     : abu_rifat
+lightoj      : Abu Rifat Muhammed Al Hasib
+facebook     : https://www.facebook.com/a.r.m.al.hasib/
+linkedin     : https://www.linkedin.com/in/abu-rifat-534258142/
+Email        : rifat.cse4.bu@gmail.com, armalhasib@gmail.com
+*/
+
+/* 
+Problem Name    : Xenia and Bit Operations
+Online Judge    : codeforces
+Solving Method  : Segment Tree
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+ll build[900005] = {};
+ll arr[900005] = {};
+
+ll buildTree(ll n, ll l, ll r, ll oppType)
+{
+    if (l == r)
+    {
+        build[n] = arr[l];
+        return build[n];
+    }
+    ll left = n * 2;
+    ll right = n * 2 + 1;
+    ll mid = (l + r) / 2;
+    ll p = buildTree(left, l, mid, !oppType);
+    ll q = buildTree(right, mid + 1, r, !oppType);
+    if (oppType == 0)
+    {
+        build[n] = p ^ q;
+    }
+    else
+    {
+        build[n] = p | q;
+    }
+    return build[n];
+}
+
+ll query(ll n, ll l, ll r, ll oppType, ll ql, ll qr, ll val)
+{
+    if (qr<l | ql> r)
+    {
+        return build[n];
+    }
+    if (ql <= l && qr >= r)
+    {
+        arr[ql] = val;
+        build[n] = val;
+        return build[n];
+    }
+    ll left = n * 2;
+    ll right = n * 2 + 1;
+    ll mid = (l + r) / 2;
+    ll p = query(left, l, mid, !oppType, ql, qr, val);
+    ll q = query(right, mid + 1, r, !oppType, ql, qr, val);
+    if (oppType == 0)
+    {
+        build[n] = p ^ q;
+    }
+    else
+    {
+        build[n] = p | q;
+    }
+    return build[n];
+}
+
+int main()
+{
+    ll level, q;
+    cin >> level >> q;
+    ll n = (1 << level);
+    ll x = (ll)(ceil(log2(n)));
+    ll oppType = (x % 2 == 0 ? 0 : 1);
+    for (ll i = 1; i <= n; i++)
+    {
+        cin >> arr[i];
+    }
+    ll sg = buildTree(1, 1, n, oppType);
+    for (ll i = 1; i <= q; i++)
+    {
+        ll index, val;
+        cin >> index >> val;
+        ll ans = query(1, 1, n, oppType, index, index, val);
+        cout << ans << endl;
+    }
+    return 0;
+}
+
+/***********Alhamdulillah***********/
